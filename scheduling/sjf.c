46d0bb17b17226607d851dd5d1607b6c5df532ca
KickItLikeShika <ahmedkhaled19701@gmail.com>
2021-12-29 17:31:05 +0200

    Organized the repo + added threads examples"

---
+++ scheduling/sjf.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+ 
+void main()
+{
+    int wt[20],tat[20],i,j,n,total=0,pos,temp;
+    int p[] = {0, 3, 4, 9};
+    int n = sizeof proc / sizeof proc[0];
+    int bt[] = {60, 30, 40, 10};
+    float avg_wt,avg_tat;
+ 
+    for(i=0;i<n;i++)
+    {
+        pos=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(bt[j]<bt[pos])
+                pos=j;
+        }
+ 
+        temp=bt[i];
+        bt[i]=bt[pos];
+        bt[pos]=temp;
+ 
+        temp=p[i];
+        p[i]=p[pos];
+        p[pos]=temp;
+    }
+ 
+    wt[0]=0;            //waiting time for first process will be zero
+ 
+    //calculate waiting time
+    for(i=1;i<n;i++)
+    {
+        wt[i]=0;
+        for(j=0;j<i;j++)
+            wt[i]+=bt[j];
+ 
+        total+=wt[i];
+    }
+ 
+    avg_wt=(float)total/n;      //average waiting time
+    total=0;
+ 
+    printf("\nProcess\t    Burst Time    \tWaiting Time\tTurnaround Time");
+    for(i=0;i<n;i++)
+    {
+        tat[i]=bt[i]+wt[i];     //calculate turnaround time
+        total+=tat[i];
+        printf("\np%d\t\t  %d\t\t    %d\t\t\t%d",p[i],bt[i],wt[i],tat[i]);
+    }
+ 
+    avg_tat=(float)total/n;     //average turnaround time
+    printf("\n\nAverage Waiting Time=%f",avg_wt);
+    printf("\nAverage Turnaround Time=%f\n",avg_tat);
+}
